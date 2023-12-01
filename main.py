a021f87fa9a51cc5d00417ae7a6987eea4d972f8
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-12-02 04:51:01 +0545

    Continuous checking for new news after a certain delay

--- main.py
+++ main.py
@@ -7,23 +7,37 @@
 
 import requests
 from bs4 import BeautifulSoup
+import time
 
-url = "https://english.onlinekhabar.com/"
-result = requests.get(url)
-soup = BeautifulSoup(result.text, "lxml")
-content = soup.find_all("div", class_ = "ok-post-contents")
+def fetch_content(): 
+    url = "https://english.onlinekhabar.com/"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "lxml")
+    return soup.find_all("div", class_ = "ok-post-contents")
 
 printed_content = set()
 
 print("Enter keyword to search for: ", end='')
 keyword = input()
 
-for i in content:
-    # keyword = 'PatAn'
-    filtered_content = i.h2.a.string.strip().lower()
-    # Here, .strip() is used to make comparision without taking whitespace in account
-    # And, .lower() is used to make all the content to lower case so the comparision is case-insensitive
+while True: 
 
-    if(keyword.lower() in filtered_content) and (filtered_content not in printed_content):
-        print(i.h2.a.string)
-        printed_content.add(filtered_content)  
+    latest_content = fetch_content()
+    new_news = False
+
+    for i in latest_content:
+        filtered_content = i.h2.a.string.strip().lower()
+        # Here, .strip() is used to make comparision without taking whitespace in account
+        # And, .lower() is used to make all the content to lower case so the comparision is case-insensitive
+
+        if(keyword.lower() in filtered_content) and (filtered_content not in printed_content):
+            print()
+            print(i.h2.a.string) 
+            printed_content.add(filtered_content)  
+            new_news = True
+        
+    if new_news: 
+        print('''\nNo more NEWS for now.\nWill inform whenever something new comes up!\n''')
+            
+    time.sleep(300)
+    # The time 300 is in seconds so it checks after every 5 minutes. 
