84a831233805746da63199919ffd7a836e66482f
YujanSubedi <0yujan0@gmail.com>
2023-12-02 14:53:18 +0545

    alert_system

--- alert.py
+++ alert.py
@@ -1,5 +1,12 @@
 import smtplib
 from email.message import EmailMessage
+import os
+from dotenv import load_dotenv
+
+# laod email and password from .env
+load_dotenv()
+Email_Address = os.getenv("email_address")
+Email_password = os.getenv("email_password")
 
 
 def alert_system(subject: str, body: str, receiver_email: str):
@@ -11,11 +18,7 @@ def alert_system(subject: str, body: str, receiver_email: str):
     with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
-        server.login("300mail.test300@gmail.com", "rbyycunmwsjdmlkg")
+        server.login(Email_Address, Email_password)
         server.send_message(alert_message)
 
     print("Alert message was sent successfully!")
-
-
-if __name__ == '__main__':
-    alert_system("test", "hello there", "0yujan0@gmail.com")
