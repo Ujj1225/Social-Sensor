b358e7dd713b606c82e235c03415a90f1c88733f
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-03-05 19:15:26 +0545

    Python Script in JS

--- controllers/model.js
+++ controllers/model.js
@@ -1,4 +1,20 @@
+const { PythonShell } = require("python-shell");
+
+let options = {
+  scriptPath: "path",
+  args: [""],
+};
+
+PythonShell.run("filename.py", options, (err, res) => {
+  if (err) {
+    console.log(err);
+  }
+  if (res) {
+    console.log(res);
+  }
+});
+
 const testing = async (req, res) => {
   res.send("Hello Baba!");
 };
-module.exports = {testing}
+module.exports = { testing };
